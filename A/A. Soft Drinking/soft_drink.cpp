8026b70d2b2a95930e78d9c6cd8cc98428b0191d
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-08-18 21:55:22 +0530

    added the soft drinking code

---
+++ A/A. Soft Drinking/soft_drink.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+
+
+int main(){
+    int n, k, l, c, d, p, nl, np;
+    std::cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    std::cout << (std::min(std::min((k * l) / nl, c * d), p / np)) / n;
+    return 0;
+}
