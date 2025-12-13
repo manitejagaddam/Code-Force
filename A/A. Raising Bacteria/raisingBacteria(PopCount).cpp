a82ca3aaa27b5ee5dd450faebf7ab7b098894929
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-12-13 11:14:17 +0530

    EcoContest 13/12/2025

---
+++ A/A. Raising Bacteria/raisingBacteria(PopCount).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    int num;
+    cin >> num;
+    int count = 0;
+    while(num){
+        count += num & 1;
+        num >>= 1;
+    }
+    cout << count;
+    return 0;
+}
