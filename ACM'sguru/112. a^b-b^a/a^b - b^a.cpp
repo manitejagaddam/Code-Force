efb0ba48408e1fd73bff391986daec0f010e39fb
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-01-02 23:05:46 +0530

    acm's guru problems

---
+++ ACM'sguru/112. a^b-b^a/a^b - b^a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int power(int a, int b){
+    long long ans = 1;
+    while(b){
+        ans *= a;
+        b--;
+    }
+    return ans;
+}
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+
+    cout << power(a, b) - power(b, a);
+    return 0;
+}
