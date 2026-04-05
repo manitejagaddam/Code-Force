f33e6aa0b4915a3342eb2b6ff48af10452deefa9
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-04-06 00:47:41 +0530

    Codeforces Round 1090 (Div. 4)

---
+++ A/A. The 67th Integer Problem/A. The 67th Integer Problem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+
+
+int main(){
+    int t;
+    std::cin >> t;
+    while(t--){
+        int n;
+        std::cin >> n;
+        if(n == 67) std::cout << 67 << std::endl;
+        else std::cout << n + 1 << std::endl;
+    }
+    return 0;
+}
